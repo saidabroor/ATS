2787c3c1b4a54469c9700674220fd37ea3da0510
saidabroor <saidabrorkhon199919@gmail.com>
2025-06-13 17:48:30 +0900

    commit

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,6 +13,11 @@ from src.utils import save_object
 
 import sys
 from dataclasses import dataclass
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
