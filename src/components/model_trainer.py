a4f4d47f17dccf70b20c6794507cc81a46a795ec
saidabroor <saidabrorkhon199919@gmail.com>
2025-06-17 14:31:39 +0900

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,7 +50,33 @@ class ModelTrainer:
       }
 
 
-      model_report:dict=evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models)
+      params = {
+    'Decision Tree': {
+        'criterion': ['gini', 'entropy', 'log_loss']
+    },
+    'Random Forest': {
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    'Gradient Boosting': {
+        'learning_rate': [0.1, 0.01, 0.05, 0.001],
+        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    'Logistic Regression': {},  # usually doesn't need much tuning
+    'XGBClassifier': {
+        'learning_rate': [0.1, 0.01, 0.05, 0.001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    'AdaBoostClassifier': {
+        'learning_rate': [0.1, 0.01, 0.05, 0.001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+}
+
+
+
+
+      model_report:dict=evaluate_models(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models, param = params)
 
       # to get the best model score from dict
       best_model_score = max(sorted(model_report.values()))
